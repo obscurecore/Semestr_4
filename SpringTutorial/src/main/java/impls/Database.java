1b928a481e1fc6763586e28c0682b50950733339
Ruslan Potapov <pruslang@gmail.com>
2020-02-02 18:01:21 +0300

    Add Robot

---
+++ SpringTutorial/src/main/java/impls/Database.java
@@ -0,0 +1,13 @@
+package impls;
+
+public class Database {
+
+	public Database() {
+		testConnection();
+	}
+
+	private void testConnection() {
+		System.out.println("Test connection!");
+	}
+
+}
---
+++ SpringTutorial/src/main/java/interfaces/Robot.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface Robot {
+	void action();
+
+	void dance();
+}
