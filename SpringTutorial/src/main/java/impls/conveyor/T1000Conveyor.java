f6718b200af46a119628f504bb1190a1706b8d26
Ruslan Potapov <pruslang@gmail.co>
2020-02-03 16:04:35 +0300

    Add method injection

---
+++ SpringTutorial/src/main/java/impls/conveyor/T1000Conveyor.java
@@ -0,0 +1,12 @@
+package impls.conveyor;
+
+
+import interfaces.Robot;
+import interfaces.RobotConveyor;
+
+public abstract class T1000Conveyor implements RobotConveyor {
+
+	@Override
+	public abstract Robot createRobot();
+
+}
---
+++ SpringTutorial/src/main/java/interfaces/RobotConveyor.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface RobotConveyor {
+
+	Robot createRobot();
+
+}
